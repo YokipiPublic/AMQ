ea0f96ef8985d6c93c9b8e524323f87b2960aff5
YokipiPublic <63680862+YokipiPublic@users.noreply.github.com>
2020-04-16 03:14:42 -0500

    Update FTFRemoteUpdate.user.js

--- FTFRemoteUpdate.user.js
+++ FTFRemoteUpdate.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         AMQ FTF Remote Update
 // @namespace    https://github.com/YokipiPublic/AMQ/
-// @version      0.1
+// @version      0.2
+// @history      0.2 Game validation and message display
 // @description  Adds a button to update spreadsheet with current score/rig
 // @author       Yokipi
 // @match        https://animemusicquiz.com/*
@@ -21,48 +22,78 @@ function addButton() {
       }
 
       // Get name, score, and rig
-      let names = [], scores = [], rigs = [], i = 0;
+      let names = [], scores = [], rigs = [], numplayers = 0, i = 0;
       for (let entryID in quiz.scoreboard.playerEntries) {
+        if (++numplayers > 2) break;
         let entry = quiz.scoreboard.playerEntries[entryID];
         names[i] = entry.$entry.find(".qpsPlayerName").text();
         scores[i] = entry.$score.text();
-        rigs[i] = entry.$entry.find(".qpsPlayerRig").text();
-        if (i++ >= 2) break;
+        rigs[i++] = entry.$entry.find(".qpsPlayerRig").text();
       }
 
-      // Create data object
-      let data = {};
-      data["P1"] = names[0];
-      data["S1"] = scores[0];
-      data["R1"] = rigs[0];
-      data["P2"] = names[1];
-      data["S2"] = scores[1];
-      data["R2"] = rigs[1];
+      // Check number of songs and players, and rig exists
+      var reject = 0;
+      if (quiz.infoContainer.$totalSongCount.text() != "15") reject = 1;
+      else if (numplayers != 2) reject = 2;
+      else if (quiz.infoContainer.$currentSongCount.text() != "15" || !quiz.infoContainer.$nameHider.hasClass('hide')) reject = 3;
+      else if (isNaN(parseInt(rigs[0])) || isNaN(parseInt(rigs[1]))) reject = 4;
 
-      // AJAX request
-      submitRequest = $.ajax({
-        url: "https://script.google.com/macros/s/AKfycbyQKN3iAJZMWL6gtn5_xPDs8ItQwHS_-A2tA6t2KkWwN6oZ8K8/exec",
-        type: "post",
-        data: data
-      });
+      if (!reject) {
+        // Create data object
+        let data = {};
+        data.P1 = names[0];
+        data.S1 = scores[0];
+        data.R1 = rigs[0];
+        data.P2 = names[1];
+        data.S2 = scores[1];
+        data.R2 = rigs[1];
 
-      // Callback handler that will be called on success
-      submitRequest.done(function (response, textStatus, jqXHR){
-          // Log a message to the console
-          console.log("Spreadsheet successfully updated!");
-      });
+        // AJAX request
+        submitRequest = $.ajax({
+            url: "https://script.google.com/macros/s/AKfycbyQKN3iAJZMWL6gtn5_xPDs8ItQwHS_-A2tA6t2KkWwN6oZ8K8/exec",
+            type: "post",
+            data: data
+        });
 
-      // Callback handler that will be called on failure
-      submitRequest.fail(function (jqXHR, textStatus, errorThrown){
-          // Log the error to the console
-          console.error("Spreadsheet failed to update: "+textStatus, errorThrown);
-      });
+        // Callback handler that will be called on success
+        submitRequest.done(function (response, textStatus, jqXHR) {
+            if (response.result == "ERROR") {
+              if (response.error.includes("repeated entry")) {
+                displayMessage("Submission rejected:\nDuplicate submission");
+                console.log("Duplicate submission");
+              } else {
+                displayMessage("Failed to submit for unknown reasons.\nServer may be busy, please try again.");
+                console.log("Rejected by GAS.");
+                console.error(response);
+              }
+            } else {
+              displayMessage("Game successfully submitted!");
+              console.log("Submission attempt successful!");
+            }
+        });
 
-      // Callback handler that will be called regardless
-      // if the request failed or succeeded
-      submitRequest.always(function () {
+        // Callback handler that will be called on failure
+        submitRequest.fail(function (jqXHR, textStatus, errorThrown) {
+            displayMessage("Failed to submit for unknown reasons.\nServer may be busy, please try again.");
+            console.error("Submission attempt failed: "+textStatus, errorThrown);
+        });
 
-      });
+        // Callback handler that will be called regardless
+        // if the request failed or succeeded
+        submitRequest.always(function () {
+
+        });
+      } else {
+        let rejectiontype = "Unknown";
+        switch (reject) {
+          case 1: rejectiontype = "Unexpected number of songs"; break;
+          case 2: rejectiontype = "Unexpected number of players"; break;
+          case 3: rejectiontype = "Game not yet finished"; break;
+          case 4: rejectiontype = "Rig not found, please make sure the rig tracker is running"; break;
+        }
+        displayMessage("Submission rejected:\n" + rejectiontype);
+        console.log("Submission rejected");
+      }
     })
     .popover({
       placement: "bottom",
